650f797590aff3d9d172f8927329de30da43ebfa
Mahirverma <mahirverma0207@gmail.com>
2024-08-17 23:09:42 +0530

    Blog_Project_First_Commit

---
+++ blogapp/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+from django.template.context_processors import request
+
+from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+
+# def home(request):
+#     context={
+#         'posts':Post.objects.all()
+#     }
+#     return render(request,'blog/home.html',context)
+#
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ["-date_posted"]
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        return False
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
